256c7f8c5d7f9c9868df87bae9b15569a8a3839e
Benjamin <benj@zagorski.com>
2017-08-23 16:56:21 -0700

    Added multiple output test

--- spec/treefuck_spec.rb
+++ spec/treefuck_spec.rb
@@ -4,6 +4,9 @@ describe "Treefuck" do
   it "can output the default value with `.`" do
     expect { Treefuck.new(".") }.to output('0').to_stdout
   end
+  it "can output several times with `..`" do
+    expect { Treefuck.new("..") }.to output('00').to_stdout
+  end
   it "can take input with `,`" do
     Treefuck.any_instance.stub(gets: "8")
     expect { Treefuck.new(",.") }.to output('8').to_stdout
