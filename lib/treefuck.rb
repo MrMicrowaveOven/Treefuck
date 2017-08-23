60beffa435ff543aecf92af57e1cb5680d1b4fd2
Benjamin <benj@zagorski.com>
2017-08-23 16:40:31 -0700

    Treefuck now sets current node to input

--- lib/treefuck.rb
+++ lib/treefuck.rb
@@ -1,10 +1,19 @@
 class Treefuck
     def initialize(command_string)
-        @val = 0
-        if command_string.chars.first == "."
+      @val = 0
+      keep_going = true
+      commands = command_string.chars
+      until commands.empty?
+        current_command = commands.shift
+        if current_command == "."
           print @val
+        elsif current_command == ","
+          @val = gets.chomp
         end
-        # command_string.chars.first
+      end
+    end
+
+    def run(command_string)
     end
 
     def left_child(command_string)
--- spec/treefuck_spec.rb
+++ spec/treefuck_spec.rb
@@ -4,7 +4,8 @@ describe "Treefuck" do
   it "can output the default value" do
     expect { Treefuck.new(".") }.to output('0').to_stdout
   end
-  # it "can output a child's value" do
-  #   expect { Treefuck.new(".") }.to output('0').to_stdout
-  # end
+  it "can take input" do
+    Treefuck.any_instance.stub(gets: "8")
+    expect { Treefuck.new(",.") }.to output('8').to_stdout
+  end
 end
