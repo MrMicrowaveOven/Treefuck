5a8c501669ef550320ad01cdfb5b69a7489176d3
Benjamin <benj@zagorski.com>
2017-08-23 23:02:56 -0700

    Switched stub to 'allow_any_instance_of' to remove deprication error

--- spec/treefuck_spec.rb
+++ spec/treefuck_spec.rb
@@ -8,7 +8,7 @@ describe "Treefuck" do
     expect { Treefuck.new("..") }.to output('00').to_stdout
   end
   it "can take input with `,`" do
-    Treefuck.any_instance.stub(gets: "8")
+    allow_any_instance_of(Treefuck).to receive(:gets).and_return("8")
     expect { Treefuck.new(",.") }.to output('8').to_stdout
   end
   it "can increment the current node with `+`" do
@@ -21,11 +21,11 @@ describe "Treefuck" do
     expect { Treefuck.new(".+.") }.to output('01').to_stdout
   end
   it "can increment an input" do
-    Treefuck.any_instance.stub(gets: "8")
+    allow_any_instance_of(Treefuck).to receive(:gets).and_return("8")
     expect { Treefuck.new(",+.") }.to output('9').to_stdout
   end
   it "can iterate inside the `[]` blocks" do
-    Treefuck.any_instance.stub(gets: "3")
+    allow_any_instance_of(Treefuck).to receive(:gets).and_return("3")
     expect { Treefuck.new(",[.-]") }.to output('321').to_stdout
   end
   it "can make left children and increment them seperately" do
@@ -41,11 +41,11 @@ describe "Treefuck" do
     expect { Treefuck.new("+<++|<|.") }.to output('1').to_stdout
   end
   it "can iterate through parent and child" do
-    Treefuck.any_instance.stub(gets: "3")
+    allow_any_instance_of(Treefuck).to receive(:gets).and_return("3")
     expect { Treefuck.new(",[>+.|-],[<+.|-]")}.to output('123123').to_stdout
   end
   it "outputs a sum of children" do
-    Treefuck.any_instance.stub(gets: "4")
+    allow_any_instance_of(Treefuck).to receive(:gets).and_return("4")
     expect { Treefuck.new("<,|>,|<[-|+<]|>[-|+>]|.") }.to output("8").to_stdout
   end
   it "can't work its way up uninitialized ancestors" do
@@ -53,10 +53,11 @@ describe "Treefuck" do
     expect { Treefuck.new("|||+|<.") }.to raise_error(StandardError)
     expect { Treefuck.new("|") }.to raise_error(StandardError)
   end
-  
+
   # Not quite sure what this example is supposed to do.
   # It's too long to work out manually, and for some reason
   # it's causing an infinite loop in my version.
+  # Meh, I'll try again later.
 
   # it "can print 'Hello World!'" do
   #   hello_world_string = "++++++++[>++++[>++>+++>+++>+||||-]>+>+>->>+[|]|-]>>.>---.+++++++..+++.>>.|-.|.+++.------.--------.>>+.>++."
